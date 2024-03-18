430307beb6fcea90b88c3ccc3b91cec4869265f7
davidagina <davidagina64@gmail.com>
2024-03-18 23:51:49 +0100

    Updated

--- src/main/java/com/railway/entity/Role.java
+++ src/main/java/com/railway/entity/Role.java
@@ -1,11 +1,30 @@
 package com.railway.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "role")
 public class Role {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
 
+    private String role;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }

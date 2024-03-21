42294680f43658472721fa4377728807d257deec
davidagina <davidagina64@gmail.com>
2024-03-21 02:14:32 +0100

    Added RailwayUserDetailsServiceImpl class

--- src/main/java/com/railway/config/RailwayUserDetailsServiceImpl.java
+++ src/main/java/com/railway/config/RailwayUserDetailsServiceImpl.java
@@ -1,2 +1,9 @@
-package com.railway.config;public class RailwayUserDetailsServiceImpl {
+package com.railway.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RailwayUserDetailsServiceImpl {
+
 }

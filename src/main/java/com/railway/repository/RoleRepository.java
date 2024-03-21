f6212dc0c05e7534f96441dc8295b4836a0ff406
davidagina <davidagina64@gmail.com>
2024-03-21 02:11:21 +0100

    Added RoleRepository interface

--- src/main/java/com/railway/repository/RoleRepository.java
+++ src/main/java/com/railway/repository/RoleRepository.java
@@ -1,2 +1,7 @@
-package com.railway.repository;public interface RoleRepository {
+package com.railway.repository;
+
+import com.railway.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
 }

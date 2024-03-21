42addb5039b2dea81a751a2a0e9973c1d29638c0
davidagina <davidagina64@gmail.com>
2024-03-21 02:17:00 +0100

    Added RailwayUserDetailsService Interface

--- src/main/java/com/railway/config/RailwayUserDetailsService.java
+++ src/main/java/com/railway/config/RailwayUserDetailsService.java
@@ -1,2 +1,9 @@
-package com.railway.config;public interface railwayUserDetailsService {
+package com.railway.config;
+
+import com.railway.entity.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface RailwayUserDetailsService extends UserDetailsService {
+
+    User save(User user);
 }
